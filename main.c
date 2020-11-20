11b7f9c3a761859a6644db309144bd990145314a
mauricio.sierrac <2376@holbertonschool.com>
2020-11-20 22:13:15 +0000

    fix main file

--- main.c
+++ main.c
@@ -9,11 +9,12 @@
 
 int main()
 {
-	char *c = "$ ", *str = NULL;
+	char *c = "$ ", *str = NULL, delim[2] = "\n", *token;
 	int command;
 	size_t n = 0;
 	struct stat st;
 	char *array[] = {NULL, NULL, "/.", NULL};
+
 
 	while (1)
 	{
@@ -21,11 +22,10 @@ int main()
 	
 		if ((command = getline(&str, &n, stdin) != -1))
 		{
-			str[command - 1] = '\0';
-
-			if(stat(str, &st) == 0)
+			token = strtok(str,delim);
+			if(stat(token, &st) == 0)
 			{
-				array[0] = str;
+				array [0] = token;
 				_execve(array);
 			}
 			else
